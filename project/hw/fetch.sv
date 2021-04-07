019a451518ae514db05300b952efb9224b2a701c
Christian-Henke <chenke@wisc.edu>
2021-04-06 19:48:36 -0500

    initial interface for fetch and decode from microdoc

--- project/hw/fetch.sv
+++ project/hw/fetch.sv
@@ -14,32 +14,23 @@ module fetch (clk, rst, in_PC_next, stall, flush, INT, INT_INST, out_PC_next, in
 	input INT;
 	input [31:0] INT_INST;
 
-	output [15:0] out_PC_next;
-	output [15:0] instr;
+	output [31:0] out_PC_next;
+	output [31:0] instr;
 	// for interrupts
 	output ACK;
 
-	wire [15:0] addr;
-	wire [15:0] PC_next;
+	wire [31:0] addr;
+	wire [31:0] PC_next;
+
+	// todo: interrupts and flush
 
 	cla_16b adder(.A(32'd4), .B(addr), .C_in(1'b0), .S(nextInstrAddr), .C_out(PC_next));
-	regDFF PC(.regQ(addr), .regD(), .clk(clk), .rst(rst), .writeEn(1'b1));
+	regDFF PC(.regQ(addr), .regD(in_PC_next), .clk(clk), .rst(rst), .writeEn(1'b1));
 	
-	
-
-	// using perfect memory
-	//memory2c instructionMem(.data_out(instr), .data_in(), .addr(addr), .enable(1'b1), .wr(1'b0), .createdump(halt), .clk(clk), .rst(rst));
-	//assign Done = 1'b1;
-	//assign Stall = 1'b0;
-	// test with stalling mem
-//	stallmem instructionMem_good(.DataOut(instr), .Done(Done), .Stall(Stall), .CacheHit(), .err(err), .Addr(addr), 
-//					.DataIn(), .Rd(1'b1), .Wr(1'b0), .createdump(halt), .clk(clk), .rst(rst));
+	assign out_PC_next = stall ? in_PC_next : PC_next;
 
 	// final mem
 	 mem_system instructionMem(.DataOut(instr), .Done(Done), .Stall(Stall), .CacheHit(), .err(err), .Addr(addr), 
 					.DataIn(), .Rd(1'b1), .Wr(1'b0), .createdump(halt), .clk(clk), .rst(rst));
 
-	// halt
-	
-	assign nextPC = ((halt & ~flush) | savedflush) ? addr : nextAddr;
 endmodule
