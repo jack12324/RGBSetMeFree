b8fc39430cf41e8d147d37e06c75b2c875807a8d
U-BIG_BERTHA\Jack <jacksporter24@gmail.com>
2021-04-15 17:17:25 -0500

    added broke fpu controller into a configuration and controller module

--- project/hw/FPUController.sv
+++ project/hw/FPUController.sv
@@ -1,6 +1,6 @@
-module FPUController #(COL_WIDTH = 10 )(col_new, shift_cols, filter, done, request_read, read_address, request_write, write_address, write_col_address, write_col, read_col_address, rd_buffer_sel, wr_buffer_sel, wr_en_wr_buffer, address_mem, read_col, result_pixels, stall, data);
+module FPUController #(COL_WIDTH = 10 )(col_new, shift_cols, filter, done, request_read, read_address, request_write, write_address, write_col_address, write_col, read_col_address, rd_buffer_sel, wr_buffer_sel, wr_en_wr_buffer, address_mem, read_col, result_pixels, stall, data, request_done);
 
-	input clk, rst_n, stall, mapped_data_valid;
+	input clk, rst_n, stall, mapped_data_valid, request_done;
 	input [7:0] read_col [COL_WIDTH-1:0];
 	input [7:0] result_pixels [COL_WIDTH-3:0];
 	input [31:0] data_mem;
@@ -15,10 +15,14 @@ module FPUController #(COL_WIDTH = 10 )(col_new, shift_cols, filter, done, reque
 	output [8:0] read_col_address;
 	output [31:0] address_mem;
 
-
-	typedef enum {IDLE, LOAD_FILTER, LOAD_WIDTH, LOAD_HEIGHT, LOAD_START_ADDR, LOAD_RESULT_ADDR, INITITAL_MEM_REQ1, INITIAL_MEM_REQ2, OPERATE, MEM_REQ, DONE, XXX} state_e;
-
+	typedef enum {IDLE, LOAD_CONFIG, INITITAL_MEM_REQ1, INITIAL_MEM_REQ2, OPERATE, MEM_REQ, DONE, XXX} state_e;
 	state_e state, next;
+
+	FPUConfig_if conf();
+	FPUControllerConfigurationLoader ConfLoader(.*, .config_if(conf.Loader));
+	
+	assign conf.mapped_data_valid = mapped_data_valid;
+	assign conf.data_mem = data_mem;
 
 	always_ff @(posedge clk, negedge rst_n)
 		if(!rst_n) state <= IDLE;
@@ -28,24 +32,16 @@ module FPUController #(COL_WIDTH = 10 )(col_new, shift_cols, filter, done, reque
 	always_comb begin
 		next_state = XXX;
 		case(state)
-			IDLE: if(data_mem == 32'h0001 && mapped_data_valid)				next = LOAD_FILTER;
+			IDLE: if(data_mem == 32'h0001 && mapped_data_valid)				next = LOAD_CONFIG;
 			      else									next = IDLE;			//@loopback
-			LOAD_FILTER: if(mapped_data_valid)						next = LOAD_WIDTH;
-				     else								next = LOAD_FILETER;		//@loopback	
-			LOAD_WIDTH: if(mapped_data_valid)						next = LOAD_HEIGHT;
-				    else								next = LOAD_WIDTH;		//@loopback
-			LOAD_HEIGHT: if(mapped_data_valid)						next = LOAD_START_ADDR;
-				     else								next = LOAD_HEIGHT;		//@loopback
-			LOAD_START_ADDR: if(mapped_data_valid)						next = LOAD_RESULT_ADDR;
-					 else								next = LOAD_START_ADDR;		//@loopback
-			LOAD_RESULT_ADDR: if(mapped_data_valid)						next = INITITAL_MEM_REQ1;
-					  else								next = LOAD_RESULT_ADDR;	//@loopback
+			LOAD_CONFIG: if(conf.load_config_done)						next = INITIAL_MEM_REQ1;
+					else								next = LOAD_CONFIG;		//@loopback
 			INITIAL_MEM_REQ1: if(request_done)						next = INITIAL_MEM_REQ2;
 					  else								next = INITIAL_MEM_REQ1; 	//@loopback
 			INITIAL_MEM_REQ2:								next = OPERATE;
 			OPERATE: if((width_counter == remaining width) || (width_counter == 512))	next = MEM_REQ;
 				 else if(height_counter == height)					next = DONE;
-				 else									next = OPERATE; 	//@loopback
+				 else									next = OPERATE; 		//@loopback
 			MEM_REQ:									next = OPERATE;
 			DONE:										next = IDLE;
 			default:									next = XXX;
@@ -54,6 +50,56 @@ module FPUController #(COL_WIDTH = 10 )(col_new, shift_cols, filter, done, reque
 	
 	//outputs	
 	always_ff @(posedge clk, negedge rst_n) begin
+		if(!rst_n) begin
+			shift_cols <= 0;
+			done <=0;
+			request_write <= 0;
+			request_read <= 0;
+			rd_buffer_sel <= 0;
+			wr_buffer_sel <= 0;
+			wr_en_wr_buffer <= 0;
+			col_new <= '0;
+			filter <= '0;
+			read_address <= '0;
+			write_address <= '0;
+			write_col_address <= '0;
+			write_col <= '0;
+			read_col_address <= '0;
+			address_mem <= '0;
+
+			conf.load_config_start <= 1;
+		end
+		else begin
+			shift_cols <= 0;
+			done <=0;
+			request_write <= 0;
+			request_read <= 0;
+			rd_buffer_sel <= 0;
+			wr_buffer_sel <= 0;
+			wr_en_wr_buffer <= 0;
+			col_new <= '0;
+			filter <= '0;
+			read_address <= '0;
+			write_address <= '0;
+			write_col_address <= '0;
+			write_col <= '0;
+			read_col_address <= '0;
+			address_mem <= '0;
+		
+			conf.load_config_start <= 0;
+			case(state)
+				IDLE: address_mem <= M_START_ADDRESS;
+				LOAD_CONFIG: begin
+					address_mem <= conf.address_mem;
+					conf.load_config_start <= 1;
+					end
+				INITITAL_MEM_REQ1:
+				INITIAL_MEM_REQ2:
+				OPERATE:
+				MEM_REQ:
+				DONE:
+				default: 
+			endcase
+		end
 	end	
-	
 endmodule
---
+++ project/hw/FPUControllerConfigurationLoader.sv
@@ -0,0 +1,108 @@
+module FPUControllerConfigurationLoader (clk, rst_n, FPUConfig_if config_if);
+	logic wr_filter1, wr_filter2, wr_filter3, wr_dims, wr_start_addr, wr_res_addr; 
+
+	typedef enum {IDLE, LOAD_FILTER, LOAD_DIMS, LOAD_START_ADDR, LOAD_RESULT_ADDR, DONE, XXX} state_e;
+
+	state_e state, next;
+
+	always_ff @(posedge clk, negedge rst_n)
+		if(!rst_n) state <= IDLE;
+		else state <= next;
+	
+	//decide next state	
+	always_comb begin
+		next_state = XXX;
+		case(state)
+			IDLE: if(config_if.load_config_start)						next = LOAD_FILTER1;
+			      else									next = IDLE;			//@loopback
+			LOAD_FILTER1: if(config_if.mapped_data_valid)					next = LOAD_FILTER2;
+				     else								next = LOAD_FILTER1;		//@loopback	
+			LOAD_FILTER2: if(config_if.mapped_data_valid)					next = LOAD_FILTER3;
+				     else								next = LOAD_FILTER2;		//@loopback
+			LOAD_FILTER3: if(config_if.mapped_data_valid)					next = LOAD_DIMS;
+				     else								next = LOAD_FILTER3;		//@loopback
+			LOAD_DIMS: if(config_if.mapped_data_valid)					next = LOAD_START_ADDR;
+				    else								next = LOAD_DIMS;		//@loopback
+			LOAD_START_ADDR: if(config_if.mapped_data_valid)				next = LOAD_RESULT_ADDR;
+					 else								next = LOAD_START_ADDR;		//@loopback
+			LOAD_RESULT_ADDR: if(config_if.mapped_data_valid)				next = INITITAL_MEM_REQ1;
+					  else								next = LOAD_RESULT_ADDR;	//@loopback
+			DONE:										next = IDLE;
+			default:									next = XXX;
+		endcase
+	end	
+	
+	//outputs	
+	always_ff @(posedge clk, negedge rst_n) begin
+		if(!rst_n) begin
+			wr_filter1 <= 0;
+			wr_filter2 <= 0;
+			wr_filter3 <= 0;
+			wr_dims <= 0;
+			wr_start_addr <= 0;
+			wr_res_addr <= 0;
+		end
+		else begin
+			wr_filter1 <= '0;
+			wr_filter2 <= '0;
+			wr_filter3 <= '0;
+			wr_dims <= 0;
+			wr_start_addr <= 0;
+			wr_res_addr <= 0;
+
+			case(state)
+				IDLE: config_if.address_mem <= M_START_ADDRESS;
+				LOAD_FILTER1: begin
+					config_if.address_mem <= M_FILTER_ADDRESS;
+					wr_filter1 <= 1;
+					end
+				LOAD_FILTER2: begin
+					config_if.address_mem <= M_FILTER_ADDRESS + 4;
+					wr_filter2 <= 1;
+					end
+				LOAD_FILTER3: begin
+					config_if.address_mem <= M_FILTER_ADDRESS + 8;
+					wr_filter3 <= 1;
+					end
+				LOAD_DIMS: begin
+					config_if.address_mem <= M_DIMS_ADDRESS;
+					wr_dims <= 1;
+					end
+				LOAD_START_ADDR: begin
+					config_if.address_mem <= M_START_ADDRESS;
+					wr_start_addr <= 1;
+					end
+				LOAD_RESULT_ADDR: begin
+					config_if.address_mem <= M_RESULT_ADDRESS;
+					wr_res_addr <= 1;
+					end
+				DONE:	config_if.load_config_done <= 1;
+				default: 
+			endcase
+		end
+	end	
+	
+	//registers to hold configuration values
+	always_ff @(posedge clk, negedge rst_n)begin
+		if (!rst_n) begin
+			config_if.filter <= '0;
+			config_if.image_width <= '0;
+			config_if.image_height <= '0;
+			config_if.start_address <= '0;
+			config_if.result_address <= '0;
+		end
+		else begin
+			if(wr_filter1) config_if.filter[0:3] <= config_if.data_mem;
+			else if(wr_filter2) config_if.filter[4:7] <= config_if.data_mem;
+			else if(wr_filter3) config_if.filter[8] <= config_if.data_mem[31:24];
+			
+			if(wr_dims) begin
+				config_if.image_width <= config_if.data_mem[31:16];
+				config_if.image_height <= config_if.data_mem[15:0];
+			end
+			
+			if(wr_start_addr) config_if.start_address <= config_if.data_mem;
+			if(wr_res_addr) config_if.result_address <= config_if.data_mem;
+		end
+	end
+endmodule
