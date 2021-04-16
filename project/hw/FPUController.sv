64cd3f55e306dcb4fc611a24232342c148c57734
U-BIG_BERTHA\Jack <jacksporter24@gmail.com>
2021-04-16 11:23:34 -0500

    adding fsm outputs

--- project/hw/FPUController.sv
+++ project/hw/FPUController.sv
@@ -11,18 +11,17 @@ module FPUController #(COL_WIDTH = 10, MEM_BUFFER_WIDTH = 512)(shift_cols, filte
 	output [8:0] read_col_address;
 	output [31:0] address_mem;
 
-	typedef enum {IDLE, LOAD_CONFIG, INITITAL_MEM_REQ1, INITIAL_MEM_REQ2_SAME_ROW, INITIAL_MEM_REQ2_NEW_ROW, OPERATE_NEW_ROW, OPERATE_CONTINUE, ROW_DONE_CONTINUE, WRITE_CATCHUP, ROW_DONE_NEW, FINAL_REQUEST, WAIT_WRITE, DONE, XXX} state_e;
+	typedef enum {IDLE, LOAD_CONFIG, FILL_BUFF1, FILL_BUFF2_ALL, FILL_BUFF2_CHUNK, NEW_ROW, OPERATE, CHUNK_END, ROW_END, ROW_DONE, CHUNK_DONE, FINAL_REQUEST, WAIT_FINAL, DONE, XXX} state_e;
 	state_e state, next;
 
 	FPUConfig_if conf();
 	FPUControllerConfigurationLoader ConfLoader(.*, .config_if(conf.Loader));
 
-	logic [17:0] total_width;
-	logic [17:0] width_counter;
-	logic [15:0] height_counter;
+	logic [17:0] total_width, remaining_width;
+	logic [15:0] remaining_height;
 
-	//width and height counter enables
-	logic w_inc_en, h_inc_en;
+	//read and write increment enables, resets
+	logic write_inc, read_inc, write_rst, read_rst;
 	
 	assign conf.mapped_data_valid = mapped_data_valid;
 	assign conf.data_mem = data_mem;
@@ -41,40 +40,43 @@ module FPUController #(COL_WIDTH = 10, MEM_BUFFER_WIDTH = 512)(shift_cols, filte
 			IDLE: if(data_mem == 32'h0001 && mapped_data_valid)						next = LOAD_CONFIG;
 			      else											next = IDLE;				//@loopback
 
-			LOAD_CONFIG: if(conf.load_config_done)								next = INITIAL_MEM_REQ1;
+			LOAD_CONFIG: if(conf.load_config_done)								next = FILL_BUFF1;
 					else										next = LOAD_CONFIG;			//@loopback
 
-			INITIAL_MEM_REQ1: if(!making_request && (total_width > MEM_BUFFER_WIDTH))			next = INITIAL_MEM_REQ2_SAME_ROW;
-					  else if(!making_request && (total_width <= MEM_BUFFER_WIDTH))        		next = INITIAL_MEM_REQ2_NEW_ROW;
-					  else										next = INITIAL_MEM_REQ1; 		//@loopback
+			FILL_BUFF1: if(!making_request && (total_width > MEM_BUFFER_WIDTH))				next = FILL_BUFF2_ALL;
+					  else if(!making_request && (total_width <= MEM_BUFFER_WIDTH))        		next = FILL_BUFF2_CHUNK;
+					  else										next = FILL_BUFF1;	 		//@loopback
 
-			INITIAL_MEM_REQ2_SAME_ROW:									next = OPERATE_NEW_ROW;
+			FILL_BUFF2_ALL:											next = NEW_ROW;
 
-			INITIAL_MEM_REQ2_NEW_ROW:									next = OPERATE_NEW_ROW;
+			FILL_BUFF2_CHUNK:										next = NEW_ROW;
 
-			OPERATE_NEW_ROW: if(width_counter == 4) 							next = OPERATE_CONTINUE;
-				 	else										next = OPERATE_NEW_ROW;			//@loopback
+			NEW_ROW: if(read_col_address == 4) 								next = OPERATE;
+				 	else										next = NEW_ROW;				//@loopback
 
-			OPERATE_CONTINUE: if(count == remaining_width)							next = WRITE_CATCHUP;
-				else if(count == MEM_BUFFER_WIDTH)							next = ROW_DONE_CONTINUE;
-				else											next = OPERATE_CONTINUE;		//@loopback
+			OPERATE: if(read_col_address == remaining_width)						next = ROW_END;
+				else if(read_col_address == MEM_BUFFER_WIDTH)						next = CHUNK_END;
+				else											next = OPERATE;				//@loopback
 
-			ROW_DONE_CONTINUE: if(!making_request)								next = OPERATE_CONTINUE;
-				else											next = ROW_DONE_CONTINUE;		//@loopback
+			CHUNK_END: if(!making_request && write_col_address == MEM_BUFFER_WIDTH-1)			next = CHUNK_DONE;
+				else if (making_request)								next = ROW_DONE;			
+				else											next = CHUNK_END;			//@loopback
+
+			CHUNK_DONE: 											next = OPERATE;
 	
-			WRITE_CATCHUP: if(write_count == remaining_width-2)begin
-					if(height_count < conf.image_height)						next = ROW_DONE_NEW;
+			ROW_END: if(write_count == remaining_width-2)begin
+					if(height_count < conf.image_height)						next = ROW_DONE;
 					else										next = FINAL_REQUEST;
-				end else										next = WRITE_CATCHUP;			//@loopback
+				end else										next = ROW_END;				//@loopback
 
-			ROW_DONE_NEW: if(!making_request)								next = OPERATE_NEW_ROW;
-				else											next = ROW_DONE_NEW;			//@loopback
+			ROW_DONE: if(!making_request)									next = NEW_ROW;
+				else											next = ROW_DONE;			//@loopback
 	
-			FINAL_REQUEST:	if(!making_request)								next = WAIT_WRITE;
+			FINAL_REQUEST:	if(!making_request)								next = WAIT_FINAL;
 				else											next = FINAL_REQUEST;			//@loopback
 	
-			WAIT_WRITE: if(!making_request)									next = DONE;
-				else											next = WAIT_WRITE;			//@loopback
+			WAIT_FINAL: if(!making_request)									next = DONE;
+				else											next = WAIT_FINAL;			//@loopback
 				
 			DONE:												next = IDLE;
 
@@ -94,31 +96,31 @@ module FPUController #(COL_WIDTH = 10, MEM_BUFFER_WIDTH = 512)(shift_cols, filte
 			wr_en_wr_buffer <= 0;
 			read_address <= '0;
 			write_address <= '0;
-			write_col_address <= '0;
-			write_col <= '0;
-			read_col_address <= '0;
 			address_mem <= '0;
 
-			conf.load_config_start <= 1;
-			inc_en <= 1;
+			conf.load_config_start <= 0;
+			write_inc <= 0;
+			read_inc <= 0;
+			read_rst <= 0;
+			write_rst <= 0;
 		end
 		else begin
 			shift_cols <= 0;
 			done <=0;
 			request_write <= 0;
 			request_read <= 0;
-			rd_buffer_sel <= 0;//
-			wr_buffer_sel <= 0;//
+			rd_buffer_sel <= rd_buffer_sel;
+			wr_buffer_sel <= wr_buffer_sel;
 			wr_en_wr_buffer <= 0;
 			read_address <= '0;
 			write_address <= '0;
-			write_col_address <= '0;//
-			write_col <= '0;
-			read_col_address <= '0;//
 			address_mem <= '0;
 		
-			inc_en <= 1;
 			conf.load_config_start <= 0;
+			write_inc <= 0;
+			read_inc <= 0;
+			read_rst <= 0;
+			write_rst <= 0;
 
 			case(state)
 				IDLE: address_mem <= M_START_ADDRESS;
@@ -126,33 +128,81 @@ module FPUController #(COL_WIDTH = 10, MEM_BUFFER_WIDTH = 512)(shift_cols, filte
 					address_mem <= conf.address_mem;
 					conf.load_config_start <= 1;
 				end
-				INITITAL_MEM_REQ1: begin
+				FILL_BUFF1: begin
 					request_read <= 1;
 					read_address <= conf.start_address;
 				end
-				INITIAL_MEM_REQ2_SAME_ROW: begin
+				FILL_BUFF2_ALL: begin
 					request_read <= 1;	
 					read_address <= conf.start_address + MEM_BUFFER_WIDTH;
+					read_rst <= 1;
+					write_rst <= 1;
 				end
-				INITIAL_MEM_REQ2_NEW_ROW: begin
+				FILL_BUFF2_CHUNK: begin
 					request_read <= 1;	
 					read_address <= conf.start_address + (total_width * COL_WIDTH);
+					read_rst <= 1;
+					write_rst <= 1;
+				end
+				NEW_ROW: begin
+					shift_cols <= 1;
+					read_inc <= 1;
 				end
 				OPERATE: begin
 					shift_cols <= 1;
+					read_inc <= 1;
+					write_inc <= 1;
 					wr_en_wr_buffer <= 1;
-					inc_en <= 1;
 				end
-				MEM_REQ:
-				DONE:
+				CHUNK_END: begin
+					shift_cols <= 1;
+					read_inc <= 1;
+					write_inc <= 1;
+					wr_en_wr_buffer <= 1;
+					rd_buffer_sel <= !wr_buffer_sel;
+				end 
+				CHUNK_DONE: begin
+					shift_cols <= 1;
+					read_inc <= 1;
+					write_inc <= 1;
+					wr_en_wr_buffer <= 1;
+					wr_buffer_sel <= rd_buffer_sel;
+					request_read <= 1;
+					request_write <= 1;
+					read_address <= todo;
+					write_address <= todo;
+				end
+				ROW_END: begin
+					shift_cols <= 1;
+					write_inc <= 1;
+					wr_en_wr_buffer <= 1;
+				end 
+				ROW_DONE: begin
+					rd_buffer_sel <= rd_buffer_sel;
+					wr_buffer_sel <= wr_buffer_sel;
+					request_read <= 1;
+					request_write <= 1;
+					read_address <= todo;
+					write_address <= todo;
+				end
+				FINAL_REQUEST: begin
+					request_write <= 1;
+					write_address <= todo;
+				end
+				WAIT_FINAL:
+				DONE:	done <= 1;
 				default: 
 			endcase
 		end
 	end	
 
 	always_ff @(posedge clk, negedge rst_n) begin
-		if(!rst_n) width_counter <= 0;
-		else if(inc_en) width_counter += 1;
+		if(!rst_n | read_rst) read_col_address <= 0;
+		else if (read_inc) read_col_address += 1;
+	end
+	always_ff @(posedge clk, negedge rst_n) begin
+		if(!rst_n | write_rst) write_col_address <= 0;
+		else if (write_inc) write_col_address += 1;
 	end
 	
 	assign read_col_address
