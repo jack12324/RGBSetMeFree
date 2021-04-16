cfaa158b5c0e2588dc3f6c2d1e7b41c144b2c333
alherreg <47951476+alherreg@users.noreply.github.com>
2021-04-15 19:23:15 -0500

    Update InterruptController.sv
    No debugging yet

--- project/hw/InterruptController.sv
+++ project/hw/InterruptController.sv
@@ -1,7 +1,29 @@
+// Addresses for routines 
+
 module InterruptController(clk, rst_n, IO, ACK, INT, INT_INSTR, IMR_in);
+
+    /**
+    Addresses to where the interrupt service routines 
+    are stored. Used to send jump instructions.
+
+    All ISRs are 0.5KB at the most (about 100 instructions)
+
+    First ISR is at 16'h0000
+    Second is at    16'h
+    **/
+    parameter INSTR_IO_0 = 32'hA0000000; 
+    parameter INSTR_IO_1 = 32'hA0000002; 
+    parameter INSTR_IO_2 = 32'hA0000004; 
+    parameter INSTR_IO_3 = 32'hA0000006; 
+    parameter INSTR_IO_4 = 32'hA0000008; 
+    parameter INSTR_IO_5 = 32'hA000000A; 
+    parameter INSTR_IO_6 = 32'hA000000C; 
+    parameter INSTR_IO_7 = 32'hA000000E; 
+    parameter INSTR_NOOP = 32'h00000000;
 
     input clk; // System clock 
     input rst_n; // Active low reset   
+
     input ACK; // Interrupt Acknowledge signal from the CPU 
     input [7:0] IO; // IO request signals. Direct connection to IO device 
     input [7:0] IMR_in; // input to the IMR register
@@ -14,6 +36,12 @@ module InterruptController(clk, rst_n, IO, ACK, INT, INT_INSTR, IMR_in);
     reg [7:0] ISR; // Interrupt Service Register (more ingormation of ff block)
     reg [7:0] ISR_in; // Input to the ISR register. Set by priority logic 
     reg [7:0] clr; // This signal clears the Signal request in the IRR
+
+    // States for the control logic stae machine 
+    typedef enum reg [2:0] {IDLE, SERV_REQ, NOOPS, INSTR, WAIT, CLR} state_t;
+	state_t state, next_state;
+
+    reg [2:0] counter; // Counter for the NoOps to be sent out to the CPU
 
     /** 
     Priority Logic
@@ -54,9 +82,93 @@ module InterruptController(clk, rst_n, IO, ACK, INT, INT_INSTR, IMR_in);
     ------
     This logic block is a big state machine to determine 
     the outputs of the 
+
+    State Descriptions:
+    - IDLE: Waiting for an interrupt request 
+    - SERV_REQ: Interrupt request has been received and 
+                need to assert the INT signal to CPU
+    - NOOPS: Sending 5 NoOps to flush the pipeline 
+    - INSTR: Sending jump instruction to CPU
+    - WAIT: Waiting for CPU to execute the service routine 
+    - CLR: Clear interrupt request 
     */
     always_comb begin
-        
+        // Setting the default values to avoid infered latches 
+        INT = 1'b0;
+        next_state = IDLE;
+        INT_INSTR = INSTR_NOOP;
+        clr = 8'b0000000;
+
+        case (state) 
+
+            // Update to ISR: IDLE --> SERV_REQ
+            IDLE : begin
+                if (|ISR) begin 
+                    next_state = SERV_REQ;
+                end
+            end
+
+            // ACK == 0: SERV_REQ --> SERV_REQ 
+            // ACK == 1: SERV_REQ --> NOOPS 
+            SERV_REQ : begin
+               INT = 1'b1;
+
+               if (ACK == 1'b0) begin
+                   next_state = SERV_REQ;
+               end 
+               else begin 
+                   next_state = SERV_REQ;
+               end 
+            end
+
+            // Have sent 5 Cycles of NoOps: NOOPS --> INSTR
+            // Less than five NoOps sent:   NOOPS --> NOOPS
+            NOOPS : begin
+                INT_INSTR = INSTR_NOOP;
+                if (counter == 3'b101) begin
+                    INT_INSTR = INSTR_NOOP;
+                end
+                else begin
+                    next_state = NOOPS;
+                end
+            end
+
+            // Unconsitional transition after 1 cycle: INSTR --> WAIT
+            INSTR : begin
+                next_state = WAIT;
+                clr = ISR;
+                
+            end
+
+            // ACK == 0: WAIT --> WAIT 
+            // ACK == 1: WAIT --> CLR 
+            WAIT : begin
+                if (ACK == 1'b0) begin
+                   next_state = WAIT;
+               end 
+               else begin 
+                   next_state = CLR;
+               end 
+            end
+
+            // Unconsitional transition after 1 cycle: CLR --> IDLE
+            CLR : begin
+                next_state = WAIT;
+
+                case (ISR)
+                    8'b00000001 : INT_INSTR = INSTR_IO_0;
+                    8'b00000010 : INT_INSTR = INSTR_IO_1;
+                    8'b00000100 : INT_INSTR = INSTR_IO_2;
+                    8'b00001000 : INT_INSTR = INSTR_IO_3;
+                    8'b00010000 : INT_INSTR = INSTR_IO_4;
+                    8'b00100000 : INT_INSTR = INSTR_IO_5;
+                    8'b01000000 : INT_INSTR = INSTR_IO_6;
+                    8'b10000000 : INT_INSTR = INSTR_IO_7;
+                endcase
+            end
+
+        endcase
+
     end
 
     /**
@@ -118,6 +230,33 @@ module InterruptController(clk, rst_n, IO, ACK, INT, INT_INSTR, IMR_in);
             ISR <= {7{1'b0}};
         else
             ISR <= ISR_in;
+    end 
+
+    /**
+    Control logic Statem Machine Flip Flop
+    ------
+    This flip flop is used to clock synchronize the state machine
+    in the control logic. (only update once a cycle)
+    **/
+    always_ff @(posedge clk, negedge rst_n) begin 
+        if (~rst_n)
+            state <= IDLE;
+        else 
+            state <= next_state;
+    end 
+
+    /**
+    Counter for the NoOps 
+    ------
+    Used to count the cycles sending NoOps. 
+    Counter won't go over 5 and it will only count when in the NOOP state.
+    **/
+    always @(posedge clk, negedge rst_n) begin 
+        if (~rst_n)
+            counter <= 3'b000;
+        else 
+            // Only count when the control logic is sending NoOps
+            counter <= (state == NOOPS) ? ((counter == 3'b101) ? 3'b101 : (counter + 1'b1)) : 3'b000;
     end 
 
 endmodule 
