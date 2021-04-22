8fcdb7b22860a8e53da5de8ab98d665e363231c9
Christian-Henke <chenke@wisc.edu>
2021-04-22 18:08:40 -0500

    update core instructions example

--- project/sw/class/example/core_instructions.s
+++ project/sw/class/example/core_instructions.s
@@ -21,43 +21,42 @@ and $r4, $r1, $r3 ; $r4 = 300
 neg $r5, $r4 ; $r5 = -300
 
 ; Shifts
-sll $r2, $r9, ? ; $r2 = 1<<?
-srl $r4, $a1, 1 ; $s2 = 1
+sll $r2, $r9, $r9 ; $r2 = 1<<1 = 2
+slr $r3, $r2, $r9 ; $r3 = 2>>1 = 1
+sar $r4, $r1, $R0 ; $r4 = 300>>0 = 300
 
 ; Stores
-sb $a0, 1024($zero)
-sh $a0, 2048($zero)
-sw $a0, 3072($zero)
+st $r1, $r1 ; this might be bad address, idk
+sti $r4, 1024 ; this might be bad address, idk
 
 ; Loads
-lbu $t1, 0x1024($zero)
-lhu $t2, 0x2048($zero)
-lw $t3, 0x3072($zero)
+ld $r5, $r1 ; 300
+ldi $r6, 1024 ; 300
 
-; Branches and CV Setting
-slt $s3, $s1, $s2 ; $s3 = 0
-slti $s4, $s1, 0x100 ; $s4 = 1
-sltiu $s5, $zero, -1 ; $s5 = 1 - Note in unsigned -1 is the largest integer
-sltu $s6, $zero, $a0 ; $s6 = 1 - same as previous
+; Branches and
+;slt $s3, $s1, $s2 ; $s3 = 0
+;slti $s4, $s1, 0x100 ; $s4 = 1
+;sltiu $s5, $zero, -1 ; $s5 = 1 - Note in unsigned -1 is the largest integer
+;sltu $s6, $zero, $a0 ; $s6 = 1 - same as previous
 
-beq $v0, $v0, SKIP_1
-ori $k0, $zero, -1 ; error condition
+;beq $v0, $v0, SKIP_1
+;ori $k0, $zero, -1 ; error condition
 
 SKIP_1:
-bne $s3, $s5, SKIP_2
-ori $k0, $zero, -1
+;bne $s3, $s5, SKIP_2
+;ori $k0, $zero, -1
 
 TMP:
-jr $ra
-ori $k0, $zero, -1
+;jr $ra
+;ori $k0, $zero, -1
 
 SKIP_2:
-j SKIP_3
-ori $k0, $zero, -1
+;j SKIP_3
+;ori $k0, $zero, -1
 
 SKIP_3:
-jal TMP
-sll $zero, $zero, 0 ; No-op
-sll $zero, $zero, 0 ; No-op
-sll $zero, $zero, 0 ; No-op
-sll $zero, $zero, 0 ; No-op
+;jal TMP
+;sll $zero, $zero, 0 ; No-op
+;sll $zero, $zero, 0 ; No-op
+;sll $zero, $zero, 0 ; No-op
+;sll $zero, $zero, 0 ; No-op
