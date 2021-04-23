9a457ec22171520a0a86412aa9b8391a7716a13b
newsatit <satitsumpun@wisc.edu>
2021-04-22 19:20:18 -0500

    demo v1  (write + load image from 0x0)

---
+++ class/demo/main.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import numpy as np
+import cv2
+
+# file names for input, output image and input filter
+# these file will be loaded to/from shared host memory
+input_img_fpath = 'input.txt'
+input_filter_fpath = 'filter.txt'
+output_img_fpath = 'output.txt'
+
+input_filter = np.array([
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+])
+
+img = cv2.imread('test.jpeg')
+
+# add zero padding
+img = np.pad(img, ((1, 1), (1,1), (0, 0)))
+shape = img.shape
+
+# save image data as bytes
+with open(input_img_fpath, 'wb') as f:
+  f.write(img.flatten().tobytes())
+
+# cload script to interact with fpga
+# shutil.copy(input_img_fpath, output_img_fpath) # mock fpga interface
+os.system('../bin/cload_sim -i script.cload')
+
+# read image data back from output file
+with open(output_img_fpath, 'rb') as f:
+  # read back as unsigned byte array
+  output = np.frombuffer(f.read(), dtype='B')
+  print(output.shape)
+  # reshape to original image shape
+  output = np.reshape(output, shape)
+  cv2.imshow('output', output)
+  cv2.waitKey()
+
+# clean up intemediary files
+for file in [input_img_fpath, output_img_fpath]:
+  os.remove(file)
