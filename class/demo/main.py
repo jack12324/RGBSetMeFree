205ceb24ebe3213a56b723eff7a61e3fad586966
newsatit <satitsumpun@wisc.edu>
2021-04-25 17:21:01 -0500

    Write size and filter data

--- class/demo/main.py
+++ class/demo/main.py
@@ -6,6 +6,7 @@ import cv2
 # file names for input, output image and input filter
 # these file will be loaded to/from shared host memory
 input_img_fpath = 'input.txt'
+img_size_fpath = 'size.txt'
 input_filter_fpath = 'filter.txt'
 output_img_fpath = 'output.txt'
 
@@ -20,10 +21,22 @@ img = cv2.imread('test.jpeg')
 # add zero padding
 img = np.pad(img, ((1, 1), (1,1), (0, 0)))
 shape = img.shape
+height, width, _ = shape
+print(height, width)
 
 # save image data as bytes
 with open(input_img_fpath, 'wb') as f:
   f.write(img.flatten().tobytes())
+
+# save image size
+with open (img_size_fpath, 'wb') as f:
+  # convert height, width to 2 bytes big endian each
+  data = height.to_bytes(2, 'big') + width.to_bytes(2, 'big')
+  f.write(data)
+
+# save filter data as bytes
+with open(input_filter_fpath, 'wb') as f:
+  f.write(input_filter.flatten().tobytes())
 
 # cload script to interact with fpga
 # shutil.copy(input_img_fpath, output_img_fpath) # mock fpga interface
@@ -38,7 +51,3 @@ with open(output_img_fpath, 'rb') as f:
   output = np.reshape(output, shape)
   cv2.imshow('output', output)
   cv2.waitKey()
-
-# clean up intemediary files
-for file in [input_img_fpath, output_img_fpath]:
-  os.remove(file)
