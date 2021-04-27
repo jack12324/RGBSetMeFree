d02b76b7d4597ee7f4d1ccf9943991f0668700be
alherreg <47951476+alherreg@users.noreply.github.com>
2021-04-27 18:07:58 -0500

    Update cpu.sv

--- project/hw/cpu.sv
+++ project/hw/cpu.sv
@@ -10,7 +10,6 @@ module cpu(clk, rst_n);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////// SIGNALS TO THE SYSTEM /////////////////////////////////////////
-
     logic flush; 
 
     /////////////////////////////////////////////////////////////////////////////
@@ -29,7 +28,7 @@ module cpu(clk, rst_n);
     //////////////////// FeDe Pipeline Register Signals /////////////////////////
 
     // Outputs to the next stage in the pipeline 5
-    output [31:0] FeDe_out_PC_next;
+    logic [31:0] FeDe_out_PC_next;
 	logic [31:0] FeDe_out_instr;
 	
     /////////////////////////////////////////////////////////////////////////////
@@ -37,14 +36,13 @@ module cpu(clk, rst_n);
 
     /////////////////////////////////////////////////////////////////////////////
     ///////////////////////////// Decode Signals ////////////////////////////////
-	input logic [31:0] reg_wrt_data; // also used in write back 
+	logic [31:0] reg_wrt_data; // also used in write back 
     // All other signals are declared in DeEx
     /////////////////////////////////////////////////////////////////////////////
     
 
     /////////////////////////////////////////////////////////////////////////////
     //////////////////// DeEx Pipeline Register Signals /////////////////////////
-
     logic [31:0] DeEx_in_PC_next;
 	logic [31:0] DeEx_in_reg_1_data;
 	logic [31:0] DeEx_in_reg_2_data;
@@ -102,12 +100,17 @@ module cpu(clk, rst_n);
 	logic [1:0] DeEx_out_result_sel;
 	logic DeEx_out_reg_wrt_en;
 	logic [4:0] DeEx_out_reg_wrt_sel;
+    /////////////////////////////////////////////////////////////////////////////
 
+
+    /////////////////////////////////////////////////////////////////////////////
+    //////////////////////////// Execute Signals ////////////////////////////////
+    input [1:0] forward1_sel,
+    input [1:0] forward2_sel, // Both from the forwarding unit 
     /////////////////////////////////////////////////////////////////////////////
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 
@@ -134,8 +137,6 @@ module cpu(clk, rst_n);
     /////////////////////////////////////////////////////////////////////////////
 
 
-
-
     /////////////////////////////////////////////////////////////////////////////
     /////////////////// Fetch - Decode Pipeline registers ///////////////////////
     FeDe iFEDE(
@@ -151,7 +152,6 @@ module cpu(clk, rst_n);
         .FeDe_out_instr(FeDe_out_instr), //[31:0]
     );
     /////////////////////////////////////////////////////////////////////////////
-
 
 
     /////////////////////////////////////////////////////////////////////////////
@@ -205,70 +205,114 @@ module cpu(clk, rst_n);
     /////////////////////////////////////////////////////////////////////////////
     ///////////////////// Decode - Execute Pipeline Regs ////////////////////////
     DeEx iDEEX(
-    //////////////////////////// Inputs /////////////////////////////
-    .clk(clk),
-    .rst_n(rst_n),
-	.flush(flush),
-    .DeEx_in_PC_next(DeEx_in_PC_next), // [31:0]
-	.DeEx_in_reg_1_data(DeEx_in_reg_1_data), // [31:0]
-	.DeEx_in_reg_2_data(DeEx_in_reg_1_data), // [31:0]
-	.DeEx_in_imm(DeEx_in_imm), // [31:0]
-	.flush(flush),
-	// special register stuff
-	.DeEx_in_LR(DeEx_in_LR), // [31:0]
-	.DeEx_in_FL(DeEx_in_FL), // [1:0]
-	// control for special registers
-	.DeEx_in_LR_read(DeEx_in_LR_read),
-	.DeEx_in_LR_write(DeEx_in_LR_write),
-	.DeEx_in_FL_read(DeEx_in_FL_read),
-	.DeEx_in_FL_write(DeEx_in_FL_write),
-	//control for forwarding 
-	.DeEx_in_reg_1_sel(DeEx_in_reg_1_sel), // [4:0]
-	.DeEx_in_reg_2_sel(DeEx_in_reg_2_sel), // [4:0]
-	// control for Execute
-	.DeEx_in_ALU_src(DeEx_in_ALU_src), // [1:0]
-	.DeEx_in_ALU_OP(DeEx_in_ALU_OP), // [4:0]
-	.DeEx_in_Branch(DeEx_in_Branch),
-	.DeEx_in_Jump(DeEx_in_Jump),
-	// control for Memory
-	.DeEx_in_mem_wrt(DeEx_in_mem_wrt),
-	.DeEx_in_mem_en(DeEx_in_mem_en),
-	// control for Writeback
-	.DeEx_in_result_sel(DeEx_in_result_sel), // [1:0]
-	.DeEx_in_reg_wrt_en(DeEx_in_reg_wrt_en),
-	.DeEx_in_reg_wrt_sel(DeEx_in_reg_wrt_sel), // [4:0]
+        //////////////////////////// Inputs /////////////////////////////
+        .clk(clk),
+        .rst_n(rst_n),
+        .flush(flush),
+        .DeEx_in_PC_next(DeEx_in_PC_next), // [31:0]
+        .DeEx_in_reg_1_data(DeEx_in_reg_1_data), // [31:0]
+        .DeEx_in_reg_2_data(DeEx_in_reg_1_data), // [31:0]
+        .DeEx_in_imm(DeEx_in_imm), // [31:0]
+        .flush(flush),
+        // special register stuff
+        .DeEx_in_LR(DeEx_in_LR), // [31:0]
+        .DeEx_in_FL(DeEx_in_FL), // [1:0]
+        // control for special registers
+        .DeEx_in_LR_read(DeEx_in_LR_read),
+        .DeEx_in_LR_write(DeEx_in_LR_write),
+        .DeEx_in_FL_read(DeEx_in_FL_read),
+        .DeEx_in_FL_write(DeEx_in_FL_write),
+        //control for forwarding 
+        .DeEx_in_reg_1_sel(DeEx_in_reg_1_sel), // [4:0]
+        .DeEx_in_reg_2_sel(DeEx_in_reg_2_sel), // [4:0]
+        // control for Execute
+        .DeEx_in_ALU_src(DeEx_in_ALU_src), // [1:0]
+        .DeEx_in_ALU_OP(DeEx_in_ALU_OP), // [4:0]
+        .DeEx_in_Branch(DeEx_in_Branch),
+        .DeEx_in_Jump(DeEx_in_Jump),
+        // control for Memory
+        .DeEx_in_mem_wrt(DeEx_in_mem_wrt),
+        .DeEx_in_mem_en(DeEx_in_mem_en),
+        // control for Writeback
+        .DeEx_in_result_sel(DeEx_in_result_sel), // [1:0]
+        .DeEx_in_reg_wrt_en(DeEx_in_reg_wrt_en),
+        .DeEx_in_reg_wrt_sel(DeEx_in_reg_wrt_sel), // [4:0]
 
-    /////////// Output to the next stage in the CPU //////////////////
-    .DeEx_out_PC_next(DeEx_out_PC_next), // [31:0]
-	.DeEx_out_reg_1_data(DeEx_out_reg_1_data), // [31:0]
-	.DeEx_out_reg_2_data(DeEx_out_reg_2_data), // [31:0]
-	.DeEx_out_imm(DeEx_out_imm), // [31:0]
-	.DeEx_flush(DeEx_flush),
-	// special register stuff
-	.DeEx_out_LR(DeEx_out_LR), // [31:0]
-	.DeEx_out_FL(DeEx_out_FL), // [1:0]
-	// control for special registers
-	.DeEx_out_LR_read(DeEx_out_LR_read),
-	.DeEx_out_LR_write(DeEx_out_LR_write),
-	.DeEx_out_FL_read(DeEx_out_FL_read),
-	.DeEx_out_FL_write(DeEx_out_FL_write),
-	//control for forwarding 
-	.DeEx_out_reg_1_sel(DeEx_out_reg_1_sel), // [4:0]
-	.DeEx_out_reg_2_sel(DeEx_out_reg_2_sel), // [4:0]
-	// control for Execute
-	.DeEx_out_ALU_src(DeEx_out_ALU_src), // [1:0]
-	.DeEx_out_ALU_OP(DeEx_out_ALU_OP), // [4:0]
-	.DeEx_out_Branch(DeEx_out_Branch),
-	.DeEx_out_Jump(DeEx_out_Jump),
-	// control for Memory
-	.DeEx_out_mem_wrt(DeEx_out_mem_wrt),
-	.DeEx_out_mem_en(DeEx_out_mem_en),
-	// control for Writeback
-	.DeEx_out_result_sel(DeEx_out_result_sel), // [1:0]
-	.DeEx_out_reg_wrt_en(DeEx_out_reg_wrt_en),
-	.DeEx_out_reg_wrt_sel(DeEx_out_reg_wrt_sel) // [4:0]
+        /////////// Output to the next stage in the CPU //////////////////
+        .DeEx_out_PC_next(DeEx_out_PC_next), // [31:0]
+        .DeEx_out_reg_1_data(DeEx_out_reg_1_data), // [31:0]
+        .DeEx_out_reg_2_data(DeEx_out_reg_2_data), // [31:0]
+        .DeEx_out_imm(DeEx_out_imm), // [31:0]
+        .DeEx_flush(DeEx_flush),
+        // special register stuff
+        .DeEx_out_LR(DeEx_out_LR), // [31:0]
+        .DeEx_out_FL(DeEx_out_FL), // [1:0]
+        // control for special registers
+        .DeEx_out_LR_read(DeEx_out_LR_read),
+        .DeEx_out_LR_write(DeEx_out_LR_write),
+        .DeEx_out_FL_read(DeEx_out_FL_read),
+        .DeEx_out_FL_write(DeEx_out_FL_write),
+        //control for forwarding 
+        .DeEx_out_reg_1_sel(DeEx_out_reg_1_sel), // [4:0]
+        .DeEx_out_reg_2_sel(DeEx_out_reg_2_sel), // [4:0]
+        // control for Execute
+        .DeEx_out_ALU_src(DeEx_out_ALU_src), // [1:0]
+        .DeEx_out_ALU_OP(DeEx_out_ALU_OP), // [4:0]
+        .DeEx_out_Branch(DeEx_out_Branch),
+        .DeEx_out_Jump(DeEx_out_Jump),
+        // control for Memory
+        .DeEx_out_mem_wrt(DeEx_out_mem_wrt),
+        .DeEx_out_mem_en(DeEx_out_mem_en),
+        // control for Writeback
+        .DeEx_out_result_sel(DeEx_out_result_sel), // [1:0]
+        .DeEx_out_reg_wrt_en(DeEx_out_reg_wrt_en),
+        .DeEx_out_reg_wrt_sel(DeEx_out_reg_wrt_sel) // [4:0]
     );
     ///////////////////////////////////////////////////////////////////////////// 
+
+
+    /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////// Execute Module ////////////////////////////////// 
+    execute(
+        .clk(clk),
+        .rst_n(rst_n),
+
+        // controls:
+        input DeEx_out_Branch,
+        input DeEx_out_Jump,
+        input [4:0] DeEx_out_ALU_op,
+        input [1:0] forward1_sel,
+        input [1:0] forward2_sel, // forwarding
+
+        // From ID/EX:
+
+        input DeEx_out_mem_wrt,
+        input DeEx_out_reg_wrt_en,
+        input DeEx_out_mem_en,
+        input [1:0] DeEx_out_result_sel,
+        input [31:0] DeEx_out_PC_next,     // just the current PC
+        input [31:0] DeEx_out_reg_1,
+        input [31:0] DeEx_out_reg_2,
+        input [1:0] DeEx_out_ALU_src,      // 0 => immi
+        input [31:0] DeEx_out_imm,
+        input [1:0] DeEx_out_FL,
+        input [31:0] DeEx_out_LR,
+        input [31:0] reg_wrt_data,         // from WB
+        input [31:0] ExMe_out_alu_out,
+
+        output [31:0] ExMe_in_alu_out,
+        output [31:0] ExMe_in_PC_next,
+        output [31:0] ExMe_in_LR_wrt_data,
+        output [31:0] ExMe_in_reg_2,
+        output [1:0] ExMe_in_FL_wrt_data
+    );
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    /////////////////////////////////////////////////////////////////////////////
+    ///////////////////// Execute - Memory Pipeline Regs ////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
