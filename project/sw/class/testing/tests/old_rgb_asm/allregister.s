2eb991271738ecbd6620f84af7cc56832494a83b
Christian-Henke <chenke@wisc.edu>
2021-04-28 11:34:52 -0500

    deleted project/sw/class

--- project/sw/class/testing/tests/old_rgb_asm/allregister.s
+++
@@ -1,6 +0,0 @@
-ldi $r1, Exception
-addi $r0, $r0, 50
-add $r0, $r0, $r0
-beq $r1
-Exception:
-.exit
--- project/sw/class/testing/tests/old_rgb_asm/branch.s
+++
@@ -1,8 +0,0 @@
-ldi $r9, SYM
-
-SYM:
-    addi $r1, $r1, 1
-    addi $r2, $r0, 1000
-    sub $r3, $r1, $r2
-    bne $r9
-.exit
