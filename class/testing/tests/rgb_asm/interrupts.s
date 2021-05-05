ea354333022ebba23cbccc0c425575abba494c88
alherreg <47951476+alherreg@users.noreply.github.com>
2021-05-05 16:52:04 -0500

    slight change to interrupts.s

--- class/testing/tests/rgb_asm/interrupts.s
+++ class/testing/tests/rgb_asm/interrupts.s
@@ -1,7 +1,7 @@
 addi $r1, $R0, 1	;0
 addi $r2, $R0, 2	;1
 addi $r3, $R0, 3	;2
-addi $r4, $R0, 4	;3 Assert IO at 4th clock cycle, cpu should fininish these instructions
+addi $r4, $R0, 4	;3 
 addi $r5, $R0, 5	;4
 addi $r6, $R0, 6	;5
 addi $r7, $R0, 7 	;6
@@ -11,6 +11,16 @@ addi $r10, $R0, 10	;9
 addi $r11, $R0, 11	;10
 nop			;11
 jmp 11			;12 infinite loop
-addi $r1, $R0, 13	;13
-addi $r2, $R0, 14	;14
-jmp 11			;15 jump to infinite loop
+nop			;13
+nop			;14
+nop			;15
+nop			;16
+nop			;17
+nop			;18
+addi $r1, $R0, 13	;19
+addi $r2, $R0, 14	;20
+rin			;21
+nop
+nop
+nop
+nop 
