cd9791d3a709e656e145d318b8d5ab1318f6340a
Christian Henke <chenke@tux-60.cae.wisc.edu>
2021-05-05 17:00:26 -0500

    improved python for assembler, new .h and .bin

--- class/ASMtoBinary.py
+++ class/ASMtoBinary.py
@@ -3,6 +3,8 @@ files = os.listdir('testing/tests/rgb_asm/')
 for file in files:
 	print(file)
 	filename = file[0 : len(file) - 2] # cut off .s
+	if os.path.exists("testing/tests/rgb_bin/"+filename+".bin"):
+		os.remove("testing/tests/rgb_bin/"+filename+".bin")
 	os.system('./bin/class -i testing/tests/rgb_asm/'+filename+'.s -o testing/tests/rgb_bin/'+filename+'.bin -s bin')
 
 
--- class/ASMtoHex.py
+++ class/ASMtoHex.py
@@ -3,6 +3,8 @@ files = os.listdir('testing/tests/rgb_asm/')
 for file in files:
 	print(file)
 	filename = file[0 : len(file) - 2] # cut off .s
+	if os.path.exists("testing/tests/rbg_hex/"+filename+".h"):
+  		os.remove("testing/tests/rbg_hex/"+filename+".h")
 	os.system('./bin/class -i testing/tests/rgb_asm/'+filename+'.s -o testing/tests/rgb_hex/'+filename+'.h -s hex')
 
 
