94b3a0e1e935f98bd92e5762a959fda0e5b0378a
Christian Henke <chenke@tux-47.cae.wisc.edu>
2021-05-06 18:45:28 -0500

    new control tests out of interest

---
+++ class/testing/tests/rgb_asm/jump_register.s
@@ -0,0 +1,14 @@
+nop
+nop
+nop
+nop
+nop
+nop
+addi $r23, $R0, 600 ; magic number
+jr $r23
+nop
+nop
+nop
+nop
+nop
+
--- project/sim/csrc/filelist.cu
+++ project/sim/csrc/filelist.cu
@@ -1,39 +1,26 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _137439_archive_1.so
-_137439_archive_1.so : archive.0/_137439_archive_1.a
+ARCHIVE_OBJS += _58863_archive_1.so
+_58863_archive_1.so : archive.2/_58863_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_137439_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_58863_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../work/ase_simv.daidir//_137439_archive_1.so $@
+	@ln -sf .//../work/ase_simv.daidir//_58863_archive_1.so $@
 
 
-ARCHIVE_OBJS += _137448_archive_1.so
-_137448_archive_1.so : archive.0/_137448_archive_1.a
+ARCHIVE_OBJS += _prev_archive_1.so
+_prev_archive_1.so : archive.2/_prev_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_137448_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../work/ase_simv.daidir//_137448_archive_1.so $@
-
-
-ARCHIVE_OBJS += _137449_archive_1.so
-_137449_archive_1.so : archive.0/_137449_archive_1.a
-	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_137449_archive_1.so --whole-archive $< --no-whole-archive
-	@rm -f $@
-	@ln -sf .//../work/ase_simv.daidir//_137449_archive_1.so $@
-
-
-ARCHIVE_OBJS += _137450_archive_1.so
-_137450_archive_1.so : archive.0/_137450_archive_1.a
-	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//_137450_archive_1.so --whole-archive $< --no-whole-archive
-	@rm -f $@
-	@ln -sf .//../work/ase_simv.daidir//_137450_archive_1.so $@
+	@ln -sf .//../work/ase_simv.daidir//_prev_archive_1.so $@
 
 
 
+VCS_CU_ARC0 =_cuarc0.so
+
+VCS_CU_ARC_OBJS0 =objs/amcQw_d.o 
 
 
 O0_OBJS =
@@ -44,6 +31,12 @@ $(O0_OBJS) : %.o: %.c
 
 %.o: %.c
 	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
+
+$(VCS_CU_ARC0) : $(VCS_CU_ARC_OBJS0)
+	$(PIC_LD) -shared  -Bsymbolic  -o .//../work/ase_simv.daidir//$(VCS_CU_ARC0) $(VCS_CU_ARC_OBJS0)
+	rm -f $(VCS_CU_ARC0)
+	@ln -sf .//../work/ase_simv.daidir//$(VCS_CU_ARC0) $(VCS_CU_ARC0)
+
 CU_UDP_OBJS = \
 
 
@@ -51,7 +44,7 @@ CU_LVL_OBJS = \
 SIM_l.o 
 
 MAIN_OBJS = \
-objs/amcQw_d.o 
 
-CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
+
+CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(VCS_CU_ARC0) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
 
