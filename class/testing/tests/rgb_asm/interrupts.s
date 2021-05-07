4ec2b693f0e46b7bdb3d3c7683611b4899906304
alherreg <47951476+alherreg@users.noreply.github.com>
2021-05-06 19:33:40 -0500

    Never updated this file

--- class/testing/tests/rgb_asm/interrupts.s
+++ class/testing/tests/rgb_asm/interrupts.s
@@ -1,25 +1,25 @@
 addi $r1, $R0, 1	;0
-addi $r2, $R0, 2	;1
-addi $r3, $R0, 3	;2
-addi $r4, $R0, 4	;3 
-addi $r5, $R0, 5	;4
-addi $r6, $R0, 6	;5
-addi $r7, $R0, 7 	;6
-addi $r8, $R0, 8	;7
-addi $r9, $R0, 9 	;8
-addi $r10, $R0, 10	;9
-addi $r11, $R0, 11	;10
-nop			;11
-jmp 5			;12 infinite loop
-nop			;13
-nop			;14
-nop			;15
-nop			;16
-nop			;17
-nop			;18
-addi $r1, $R0, 13	;19
-addi $r2, $R0, 14	;20
-rin			;21
+addi $r2, $R0, 2	;4
+addi $r3, $R0, 3	;8
+addi $r4, $R0, 4	;12 
+addi $r5, $R0, 5	;16
+addi $r6, $R0, 6	;20
+addi $r7, $R0, 7 	;24
+addi $r8, $R0, 8	;28
+addi $r9, $R0, 9 	;32
+addi $r10, $R0, 10	;36
+addi $r11, $R0, 11	;40
+nop			;44
+nop			;48 infinite loop
+jmp 12			;52
+nop			;56
+nop			;60
+nop			;64
+nop			;68
+nop			;72
+addi $r1, $R0, 13	;76
+addi $r2, $R0, 14	;80
+rin			;84
 nop
 nop
 nop
